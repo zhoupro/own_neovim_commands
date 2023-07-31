1413e63d25b0caf0a9dae98ba41f7b7daebd2411
zhoushengzheng <zhoushengzheng@gmail.com>
2023-07-31 17:35:40 +0800

    feat: VisualTag Cmd

--- lua/own_neovim_commands/commands.lua
+++ lua/own_neovim_commands/commands.lua
@@ -3,6 +3,7 @@ local M = {}
 
 function M.setup()
     vim.api.nvim_create_user_command('CopyToRemote', require('own_neovim_commands').CopyToRemote, {})
+    vim.api.nvim_create_user_command('VisualTag', require('own_neovim_commands').Visual, {})
     vim.api.nvim_create_user_command('Logger', require('own_neovim_commands').GoLogger, {})
     vim.api.nvim_set_keymap('n', '<Leader>y',  [[<Cmd>lua require('own_neovim_commands').CopyToRemote()<CR>]], { noremap = true, silent = true })
 end
